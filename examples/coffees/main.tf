c9d045a18d23cb2b3f64bdf8e00683324efa1e1e
inspirit941 <inspirit941@gmail.com>
2023-12-29 12:20:58 +0900

    feat: coffees datasource 대상으로 terraform plan 수행 - 정상적으로 세팅되었는지 체크

---
+++ examples/coffees/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    hashicups = {
+      source = "hashicorp.com/edu/hashicups"
+    }
+  }
+}
+
+// provider block에서, 필요한 attribute를 정의해준다.
+// provider 코드에서 '환경변수' 로도 해당 값을 받을 수 있도록 해두었지만, 쉽게 run the cycle하기 위해 여기에 평문으로도 추가.
+provider "hashicups" {
+  host     = "http://localhost:19090"
+  username = "education"
+  password = "test123"
+}
+
+data "hashicups_coffees" "edu" {}
