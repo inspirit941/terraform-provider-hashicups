859073d102a57e5c1068f1c7bdbd1b12a1c5ae3e
inspirit941 <inspirit941@gmail.com>
2023-12-29 11:49:42 +0900

    feat: configure 로직 추가 - 공식문서에 있는 내용 복붙. - client library (hashicups API의 CRUD 기능) 와 provider가 분리돼 있음. (client library는 hashicorp의 demo app을 사용한다)

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,8 +2,11 @@ package provider
 
 import (
 	"context"
+	"os"
 
+	"github.com/hashicorp-demoapp/hashicups-client-go"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -72,6 +75,122 @@ func (p *hashicupsProvider) Schema(_ context.Context, _ provider.SchemaRequest, 
 // Configure prepares a HashiCups API client for data sources and resources.
 // -> provider가 running하기 위해 필요한 로직을 넣는 함수. 보통 authentication이 들어감.
 func (p *hashicupsProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	// Retrieve provider data from configuration
+	// -> main.tf 에서 provider "" {} block의 정보를 의미함.
+    var config hashicupsProviderModel
+    diags := req.Config.Get(ctx, &config)
+    resp.Diagnostics.Append(diags...)
+    if resp.Diagnostics.HasError() {
+        return
+    }
+
+    // If practitioner provided a configuration value for any of the
+    // attributes, it must be a known value.
+
+    if config.Host.IsUnknown() {
+        resp.Diagnostics.AddAttributeError(
+            path.Root("host"),
+            "Unknown HashiCups API Host",
+            "The provider cannot create the HashiCups API client as there is an unknown configuration value for the HashiCups API host. "+
+                "Either target apply the source of the value first, set the value statically in the configuration, or use the HASHICUPS_HOST environment variable.",
+        )
+    }
+
+    if config.Username.IsUnknown() {
+        resp.Diagnostics.AddAttributeError(
+            path.Root("username"),
+            "Unknown HashiCups API Username",
+            "The provider cannot create the HashiCups API client as there is an unknown configuration value for the HashiCups API username. "+
+                "Either target apply the source of the value first, set the value statically in the configuration, or use the HASHICUPS_USERNAME environment variable.",
+        )
+    }
+
+    if config.Password.IsUnknown() {
+        resp.Diagnostics.AddAttributeError(
+            path.Root("password"),
+            "Unknown HashiCups API Password",
+            "The provider cannot create the HashiCups API client as there is an unknown configuration value for the HashiCups API password. "+
+                "Either target apply the source of the value first, set the value statically in the configuration, or use the HASHICUPS_PASSWORD environment variable.",
+        )
+    }
+
+    if resp.Diagnostics.HasError() {
+        return
+    }
+
+    // Default values to environment variables, but override
+    // with Terraform configuration value if set.
+
+    host := os.Getenv("HASHICUPS_HOST")
+    username := os.Getenv("HASHICUPS_USERNAME")
+    password := os.Getenv("HASHICUPS_PASSWORD")
+
+    if !config.Host.IsNull() {
+        host = config.Host.ValueString()
+    }
+
+    if !config.Username.IsNull() {
+        username = config.Username.ValueString()
+    }
+
+    if !config.Password.IsNull() {
+        password = config.Password.ValueString()
+    }
+
+    // If any of the expected configurations are missing, return
+    // errors with provider-specific guidance.
+
+    if host == "" {
+        resp.Diagnostics.AddAttributeError(
+            path.Root("host"),
+            "Missing HashiCups API Host",
+            "The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API host. "+
+                "Set the host value in the configuration or use the HASHICUPS_HOST environment variable. "+
+                "If either is already set, ensure the value is not empty.",
+        )
+    }
+
+    if username == "" {
+        resp.Diagnostics.AddAttributeError(
+            path.Root("username"),
+            "Missing HashiCups API Username",
+            "The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API username. "+
+                "Set the username value in the configuration or use the HASHICUPS_USERNAME environment variable. "+
+                "If either is already set, ensure the value is not empty.",
+        )
+    }
+
+    if password == "" {
+        resp.Diagnostics.AddAttributeError(
+            path.Root("password"),
+            "Missing HashiCups API Password",
+            "The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API password. "+
+                "Set the password value in the configuration or use the HASHICUPS_PASSWORD environment variable. "+
+                "If either is already set, ensure the value is not empty.",
+        )
+    }
+
+    if resp.Diagnostics.HasError() {
+        return
+    }
+
+    // Create a new HashiCups client using the configuration values
+	// -> go get github.com/hashicorp-demoapp/hashicups-client-go@v0.1.0 으로 불러온 것. 실제로는 client도 있어야하는 듯.
+    client, err := hashicups.NewClient(&host, &username, &password)
+    if err != nil {
+        resp.Diagnostics.AddError(
+            "Unable to Create HashiCups API Client",
+            "An unexpected error occurred when creating the HashiCups API client. "+
+                "If the error is not clear, please contact the provider developers.\n\n"+
+                "HashiCups Client Error: "+err.Error(),
+        )
+        return
+    }
+
+    // Make the HashiCups client available during DataSource and Resource
+    // type Configure methods.
+    resp.DataSourceData = client
+    resp.ResourceData = client
 }
 
 // DataSources defines the data sources implemented in the provider.
